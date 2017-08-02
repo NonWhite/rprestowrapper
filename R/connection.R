a08b66f1b0741c7df6ed6686446ff86151e629bf
Renan Cruz <renanpablocruz@gmail.com>
2017-08-01 23:18:54 -0300

    fix doc

--- R/connection.R
+++ R/connection.R
@@ -2,10 +2,10 @@
 #'
 #' @param host Presto server's addres
 #' @param port Presto server's port
-#' @param port Presto server's user for making queries
-#' @param port Presto server's schema
-#' @param port Presto server's catalog
-#' @param port Presto server's source
+#' @param user Presto server's user for making queries
+#' @param schema Presto server's schema
+#' @param catalog Presto server's catalog
+#' @param source Presto server's source
 #' @export
 start_connection <- function(host,port,user,schema,catalog,source){
 	vars = list(
