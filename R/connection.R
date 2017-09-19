649c8945757bbcfb45a46aa6110f5fe4301fde0b
Walter Perez <walter.perez@tfgco.com>
2017-09-19 13:05:21 -0300

    new: user/pass authentication

--- R/connection.R
+++ R/connection.R
@@ -2,18 +2,20 @@
 #'
 #' @param host Presto server's addres
 #' @param port Presto server's port
-#' @param port Presto server's user for making queries
-#' @param port Presto server's schema
-#' @param port Presto server's catalog
-#' @param port Presto server's source
+#' @param user Presto server's user for making queries
+#' @param schema Presto server's schema
+#' @param catalog Presto server's catalog
+#' @param source Presto server's source
+#' @param password Presto server's user password
 #' @export
-start_connection <- function(host,port,user,schema,catalog,source){
+start_connection <- function(host,port,user,schema,catalog,source,password=''){
 	vars = list(
 		host = host,
 		port = port,
 		user = user,
 		schema = schema,
 		catalog = catalog,
-		source = source
+		source = source,
+		password = password
 	)
 }
--- R/query.R
+++ R/query.R
@@ -1,38 +1,51 @@
 send_query <- function(conn,sql_query){
 	url = paste(conn$host, ':', conn$port, '/v1/statement', sep = '')
 	body = gsub(';', '', sql_query)
-	r = httr::POST(url, body = body, encode = "raw",
-			httr::add_headers('X-Presto-Catalog' = conn$catalog,
-				'X-Presto-Source' = conn$source,
-				'X-Presto-Schema' = conn$schema,
-				'User-Agent' = 'rprestowrapper',
-				'X-Presto-User' = conn$user
-			)
-		)
+	headers = list(
+		'X-Presto-Catalog' = conn$catalog,
+		'X-Presto-Source' = conn$source,
+		'X-Presto-Schema' = conn$schema,
+		'User-Agent' = 'rprestowrapper'
+		'X-Presto-User' = conn$user
+	)
+	if(conn$password == ''){
+		r = httr::POST(url, body = body, encode = "raw",httr::add_headers(headers))
+	else{
+		r = httr::POST(url, body = body, encode = "raw",httr::add_headers(headers),
+			httr::authenticate(user=conn$user,password=conn$password))
+	}
+}
+
+get_rows_request <- function(nextUri){
+	res = httr::GET(nextUri)
+	parsed = jsonlite::fromJSON(httr::content(res,'text',encoding='UTF-8'))
+	keys = names(parsed)
+	if('error' %in% keys){
+		stop(parsed$error$message)
+	}
+	parsed
 }
 
 get_query_result <- function(res){
-  parsed = jsonlite::fromJSON(httr::content(res,'text', encoding='UTF-8'))
-  keys = names(parsed)
-  dataframes = list()
-  while('nextUri' %in% keys){
-    nextUri = parsed$nextUri
-    res = httr::GET(nextUri)
-    parsed = jsonlite::fromJSON(httr::content(res,'text', encoding='UTF-8'))
-    keys = names(parsed)
-    if('data' %in% keys){
-      column_names = as.list(parsed$columns[,1])
-      dataframe = data.frame(parsed$data)
-      if(nrow(dataframe) > 0){
-        colnames(dataframe) = column_names
-      }
-      dataframes = c(dataframes, list(dataframe))
-    }
-    else{
-      Sys.sleep(5)
-    }
-  }
-  r = Reduce(function(x, y) merge(x, y, all=TRUE), dataframes)
+	parsed = jsonlite::fromJSON(httr::content(res,'text',encoding='UTF-8'))
+	keys = names(parsed)
+	dataframes = list()
+	while('nextUri' %in% keys){
+		parsed = make_rows_request(parsed$nextUri)
+		keys = names(parsed)
+		if('data' %in% keys){
+			column_names = as.list(parsed$columns[,1])
+			dataframe = data.frame(parsed$data)
+			if(nrow(dataframe) > 0){
+				colnames(dataframe) = column_names
+			}
+			dataframes = c(dataframes, list(dataframe))
+		}
+		else{
+			Sys.sleep(5)
+		}
+	}
+	r = Reduce(function(x, y) merge(x,y,all=TRUE),dataframes)
 }
 
 #' Run query on presto
